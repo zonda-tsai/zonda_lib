6350fce910f58b771944905f029b21e81075a092
zonda-tsai <dallas.tsai9@gmail.com>
2025-08-10 13:21:16 +0800

    Add files via upload

---
+++ include/customise.h
@@ -0,0 +1,25 @@
+#ifndef UNICODE_CUSTOMISED_BY_ZONDA
+#define UNICODE_CUSTOMISED_BY_ZONDA
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "win_term_mode.h"
+#include "greek.h"
+#include "science.h"
+#include "special_fonts.h"
+#include "arrows.h"
+#include "box.h"
+#include "decorator.h"
+#include "escape_clean.h"
+#include "cursor.h"
+#include "check.h"
+#include "chess.h"
+#include "poker.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ include/escape_clean.h
@@ -0,0 +1,19 @@
+#ifndef ESCAPE_CHARACTER_CLEAN_H
+#define ESCAPE_CHARACTER_CLEAN_H
+
+#ifdef _cplusplus
+extern "C"{
+#endif
+
+#include "win_term_mode.h"
+
+#define _clear "\x1b[2J\x1b[1;1H"
+#define _clear_line_end "\x1b[0K"
+#define _clear_line_start "\x1b[1K"
+#define _clear_line "\x1b[2K"
+
+#ifdef _cplusplus
+}
+#endif
+
+#endif
