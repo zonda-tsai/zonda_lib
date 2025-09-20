46912c7976f669f66861158bf99136204c668134
zonda-tsai <dallas.tsai9@gmail.com>
2025-09-20 22:51:40 +0800

    Add files via upload

--- file_system/file_system.c
+++ file_system/file_system.c
@@ -137,7 +137,7 @@ bool isRead(const char* file_name){
 }
 
 bool isWrite(const char* file_name){
-	DWORD attrs = GetFileAttributeA(file_name);
+	DWORD attrs = GetFileAttributesA(file_name);
 	if(attrs == INVALID_FILE_ATTRIBUTE) return false;
 	return !(attrs & FILE_ATTRIBUTE_READONLY);
 }
