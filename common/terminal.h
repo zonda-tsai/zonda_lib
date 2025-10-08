4d9a109282a69d7e6a5c170e3f67f2f318cebcb5
zonda-tsai <dallas.tsai9@gmail.com>
2025-10-09 02:12:32 +0800

    Add files via upload

--- common/terminal.h
+++ common/terminal.h
@@ -1,6 +1,10 @@
 #ifndef TERMINAL_BRANCH
 #define TERMINAL_BRANCH
 
+#ifdef __cplusplus
+extern "C"{
+#endif
+
 #ifdef _WIN32
 extern HANDLE hstdin_win;
 extern DWORD dwOriginalMode;
@@ -15,5 +19,9 @@ void set_term_size();
 void disableRawMode();
 void enableRawMode();
 void TUI_mode();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
